52b15d0161257cd367270d09948448fb93009992
Amgalan Gerelsaikhan <gerel.amgaa@gmail.com>
2024-01-23 21:14:18 +0900

    VideoFix

--- app/page.tsx
+++ app/page.tsx
@@ -12,15 +12,15 @@ const navigation = [
 
 export default function Home() {
   return (
-    <div>
-    <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-[#e5e5e5]">
+    <div className="flex flex-col gap-10 bg-[#e5e5e5]">
+    <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden ">
       <nav className="my-16 animate-fade-in">
-        <ul className="flex items-center justify-center gap-4">
+        <ul className="flex items-center justify-center gap-10">
           {navigation.map((item) => (
             <Link
               key={item.href}
               href={item.href}
-              className="text-sm duration-75 text-[#14213d] hover:text-[#000000]"
+              className="text-base duration-75 text-[#14213d] hover:text-[#100f0f] hover:underline"
             >
               {item.name}
             </Link>
@@ -49,30 +49,33 @@ export default function Home() {
 
     </div>
 
-      <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-[#e5e5e5]">
+      <div className="flex flex-col items-center animate-fade-in">
 
-<h1 className="font-bold tracking-tight duration-75 text-[#14213d] hover:text-[#000000]
-sm:text-2xl md:text-3xl">
+<h1 className="font-bold tracking-tight duration-80 text-[#14213d] hover:text-[#000000]
+sm:text-3xl md:text-3xl">
           Heron's Way is now available on {" "}
           <Link
             target="_blank"
             href="https://www.amazon.com/Herons-Way-Adventures-Became-Chinggis-ebook/dp/B0CS6JRRYC/ref=sr_1_3?crid=30E00WFQOLEEZ&keywords=Heron%27s+way&qid=1705191079&refinements=p_n_feature_nine_browse-bin%3A3291437011&rnid=3291435011&s=books&sprefix=heron%27s+way%2Caps%2C277&sr=1-3"
             className="underline duration-500 hover:text-zinc-300"
           >
-            Amazon.
+            Amazon. Get your Copy Today!
           </Link>
         </h1>
 
-<div className="aspect-w-16 aspect-h-9 w-[800px] h-[400px] max-w-[800px]">
-        <iframe
-          className="w-full h-full"
-          src="https://www.youtube.com/embed/qlS97wcqSTw" // Replace with your YouTube video embed URL
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
-        </div>
+ 
+        <div className="aspect-w-16 aspect-h-9 relative xl:max-w-[1100px] md:w-[600px]">
+  <iframe
+    className=""
+    width="100%" height="350"
+    src="https://www.youtube.com/embed/qlS97wcqSTw" // Replace with your YouTube video embed URL
+    title="Heron's Way on Sale Now!"
+    frameBorder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowFullScreen data-gtm-yt-inspected-9="true"
+  ></iframe>
+</div>
+
              {/* Review Section */}
        <div className="bg-[#e1dede] text-[#14213d] px-20 my-5">
         <div className="text-sm mx-auto grid md:grid-cols-3 gap-6">
@@ -137,7 +140,6 @@ sm:text-2xl md:text-3xl">
 
         </div>
       </div>
-
       </div>
 
   
